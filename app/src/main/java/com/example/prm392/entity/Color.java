4d500e49b259f8c4146354011c166c8cbefaef66
thanh <thanhtnhe172748@fpt.edu.vn>
2024-10-22 00:45:09 +0700

    Fix Color class

--- app/src/main/java/com/example/prm392/entity/Color.java
+++ app/src/main/java/com/example/prm392/entity/Color.java
@@ -1,23 +1,30 @@
 package com.example.prm392.entity;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 
 import java.sql.Date;
 
 @Entity(tableName = "color")
 public class Color extends BaseEntity{
-    @ColumnInfo(name = "color", typeAffinity = ColumnInfo.INTEGER)
-    private int color;
+    @ColumnInfo(name = "color", typeAffinity = ColumnInfo.TEXT)
+    private String color;
 
-    public Color(long id, Date createdAt, Date updatedAt, Date deletedAt, String createdBy, String updatedBy, String deletedBy, int color) {
+    public Color(long id, Date createdAt, Date updatedAt, Date deletedAt, String createdBy, String updatedBy, String deletedBy, String color) {
         super(id, createdAt, updatedAt, deletedAt, createdBy, updatedBy, deletedBy);
         this.color = color;
     }
-    public int getColor() {
+    @Ignore // Room sẽ bỏ qua constructor này
+    public Color(long id,String color) {
+        super(id);
+        this.color = color;
+    }
+
+    public String getColor() {
         return color;
     }
 
-    public void setColor(int color) {
+    public void setColor(String color) {
         this.color = color;
     }
 }
